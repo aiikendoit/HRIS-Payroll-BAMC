b43abd1cea8d894be47795ed9bd58cf7930fe89d
palin01 <palinjames27@gmail.com>
2023-04-12 21:53:23 +0800

    saving message

---
+++ HRIS/Classes/Testmessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRIS.Classes
+{
+    public class Testmessage
+    {
+        public void testaccess()
+        {
+            MessageBox.Show("Hello world!");
+        }
+    }
+}

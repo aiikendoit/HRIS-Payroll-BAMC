b4fef9a7c5bb3beca23335ad3fdc3fdf80a78f8e
aiikendoit <akdisrael15@gmail.com>
2023-05-03 18:53:35 +0800

    add leave setting

---
+++ HRIS/HRIS/HRIS/Forms/Employee/Leaves/emp_leaveSetting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HRIS.Forms.Employee.Leaves
+{
+    public partial class emp_leaveSetting : Form
+    {
+        public emp_leaveSetting()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

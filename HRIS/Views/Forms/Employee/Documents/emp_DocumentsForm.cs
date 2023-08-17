3a1b2bac5ce8bab656c7b889b3f114cc961f5409
aiikendoit <akdisrael15@gmail.com>
2023-08-17 21:53:37 +0800

    add load preview docs file in employee docs

--- HRIS/Views/Forms/Employee/Documents/emp_DocumentsForm.cs
+++ HRIS/Views/Forms/Employee/Documents/emp_DocumentsForm.cs
@@ -3,6 +3,7 @@ using HRIS.Models;
 using HRIS.Presenter;
 using HRIS.Views.Forms.Employee;
 using HRIS.Views.Forms.Employee.Documents;
+using HRIS.Views.Forms.Maintenance.Degreetype;
 using HRIS.Views.Forms.Maintenance.SalarySetting;
 using System;
 using System.Collections.Generic;
@@ -12,6 +13,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Forms;
 
 namespace HRIS.Forms.Employee.Documents
@@ -31,6 +33,7 @@ namespace HRIS.Forms.Employee.Documents
             //PKEmployeeid = PKEmployeeid;
             loadEmployeeDocumentsData();
 
+            btn_viewDocs.Click += btn_viewDocs_Click;
         }
 
         private void loadEmployeeDocumentsData()
@@ -41,11 +44,12 @@ namespace HRIS.Forms.Employee.Documents
 
         private void dgv_hideCols()
         {
-            dgrid_documents.Columns[0].Visible = false;
+            //dgrid_documents.Columns[0].Visible = false;
             dgrid_documents.Columns[1].Visible = false;
             dgrid_documents.Columns[2].Visible = false;
             dgrid_documents.Columns[6].Visible = false;
             dgrid_documents.Columns[7].Visible = false;
+            dgrid_documents.Columns[5].Visible = false;
         }
 
         private void emp_DocumentsForm_Load(object sender, EventArgs e)
@@ -80,12 +84,40 @@ namespace HRIS.Forms.Employee.Documents
             //dgrid_documents.Columns[3].Width = 100;
             //dgrid_documents.Columns[1].HeaderText = "Educational Attainment";
             //dgrid_documents.Columns[2].HeaderText = "School Attended";
-            dgrid_documents.Columns[3].HeaderText = "Description";
-            dgrid_documents.Columns[4].HeaderText = "Remarks";
-            dgrid_documents.Columns[5].HeaderText = "Created Date";
-            
-            
+            //dgrid_documents.Columns[3].HeaderText = "Description";
+            //dgrid_documents.Columns[4].HeaderText = "Remarks";
+            //dgrid_documents.Columns[5].HeaderText = "Created Date";
+        }
 
+        private void btn_Edit_Click(object sender, EventArgs e)
+        {
+            if (dgrid_documents.SelectedCells.Count > 0)
+            {
+                // Get the selected value from the DataGridView
+                int selectedValue = Convert.ToInt32(dgrid_documents.SelectedCells[0].Value);
+                // Pass the selected value to Form2's constructor
+                Add_Docs form2 = new Add_Docs(selectedValue);
+                form2.ShowDialog(); // Show Form2 as a dialog
+            }
+
+        }
+
+        private void updateEmployeeDocsData()
+        {
+
+        }
+
+        private void btn_viewDocs_Click(object sender, EventArgs e)
+        {
+            
+            int PKEmployeeID = (int)dgrid_documents.SelectedRows[0].Cells[0].Value;
+            if (PKEmployeeID != null)
+            {
+                var empDocs = new PreviewDocs(PKEmployeeID);
+                //empDocs.putdata(this, selectedEmpDocs);
+                empDocs.ShowDialog(this);
+
+            }
         }
     }
 }
